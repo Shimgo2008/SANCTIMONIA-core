bfaa741e4f4990d4ea08e55139ed2d79180edd08
Shimgo2008 <Shimgo2008@gmail.com>
2026-02-02 21:09:33 +0900

    refactor: modularization

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,14 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/eigen/dense.h>
 #include <nanobind/eigen/sparse.h>
-#include <nanobind/stl/optional.h>
-#include <nanobind/stl/complex.h>
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
-#include <Eigen/IterativeLinearSolvers>
-#include <complex>
-#include <iostream>
+#include <nanobind/stl/string.h>
+
+#include "common_types.hpp"
+#include "solvers.hpp"
+#include "nn_preprocessor.hpp"
 
 namespace nb = nanobind;
-
-// Real Types
-using RowMatrixXd = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-using VectorXd = Eigen::VectorXd;
-using SparseMatrixXd = Eigen::SparseMatrix<double>;
-
-// Complex Types
-using Complex = std::complex<double>;
-using RowMatrixXcd = Eigen::Matrix<Complex, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-using VectorXcd = Eigen::VectorXcd;
-using SparseMatrixXcd = Eigen::SparseMatrix<Complex>;
-
-// --- 例外スロー用ヘルパー ---
-void throw_solver_error(const std::string& class_name, const std::string& msg, 
-                        std::optional<int> iterations = std::nullopt, 
-                        std::optional<double> error = std::nullopt) {
-    nb::gil_scoped_acquire acquire;
-
-
-    nb::handle ex_mod = nb::module_::import_("sanctimonia.types.exception");
-    nb::object ex_cls = ex_mod.attr(class_name.c_str());
-    
-    nb::object instance;
-    if (iterations && error) {
-        instance = ex_cls(msg, *iterations, *error);
-    } else {
-        instance = ex_cls(msg);
-    }
-
-    PyErr_SetObject(ex_cls.ptr(), instance.ptr());
-    throw nb::python_error();
-}
-
-void throw_decomposition_error(const std::string& msg) {
-    nb::gil_scoped_acquire acquire;
-
-    nb::object ex_mod = nb::module_::import_("sanctimonia.types.exception");
-    nb::object ex_cls = nb::borrow<nb::object>(PyObject_GetAttrString(ex_mod.ptr(), "DecompositionError"));
-    if (!ex_cls) { PyErr_Clear(); return; }
-
-    nb::object instance = ex_cls(msg);
-    PyErr_SetObject(ex_cls.ptr(), instance.ptr());
-    throw nb::python_error();
-}
-
-// --- 前処理設定 ---
-template <typename Solver>
-void configure_preconditioner(Solver& solver) {
-    using Scalar = typename Solver::Scalar;
-    if constexpr (std::is_same_v<typename Solver::Preconditioner, Eigen::IncompleteLUT<Scalar>>) {
-        solver.preconditioner().setFillfactor(10);
-        solver.preconditioner().setDroptol(1e-4);
-    }
-}
-
-// --- 共通実行ロジック ---
-template <typename Solver, typename MatrixType, typename VectorType>
-VectorType solve_impl(const MatrixType& A, const VectorType& b, std::optional<VectorType> x0, double tol) {
-    Solver solver;
-    configure_preconditioner(solver);
-    solver.setTolerance(tol);
-    
-    solver.compute(A);
-    if (solver.info() != Eigen::Success) {
-       throw_decomposition_error("Decomposition failed (info=" + std::to_string(solver.info()) + ")");
-    }
-
-    VectorType x;
-    if (x0.has_value()) {
-        x = solver.solveWithGuess(b, *x0);
-    } else {
-        x = solver.solve(b);
-    }
-    
-    if (solver.info() != Eigen::Success) {
-        throw_solver_error("ConvergenceError", "Solver failed to converge", 
-                           solver.iterations(), solver.error());
-    }
-
-    return x;
-}
+using namespace sanctimonia::core;
 
 // --- マクロ定義 ---
 // 実数・複素数の両方に対応するためのテンプレートマクロ
@@ -125,8 +43,6 @@ NB_MODULE(core, m) {
     REGISTER_SPARSE_SOLVER_TEMPLATE("solve_lscg_ilu", ILU_LSCG, RowMatrixXd, VectorXd, SparseMatrixXd);
 
     // --- Complex Solvers ---
-    // Python側では同じ関数名でオーバーロードとして扱う
-
     // CG (Hermitian Positive Definite)
     REGISTER_SOLVER_TEMPLATE("solve_cg", Eigen::ConjugateGradient<RowMatrixXcd>, RowMatrixXcd, VectorXcd);
     // BiCGSTAB (Non-Hermitian)
